83b61e654750ecc85f56cf6b586c3c3ead22b6d0
Karthik Meda <karthikreddymeda@gmail.com>
2016-10-21 19:28:15 +0530

    wiki api call

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
 
 $('input').keypress(function(ev){
-  if (ev.keyCode == '13') {
+  if (ev.keyCode == '13' && !($("input[name=wikipedia]").val() == '')) {
     $('.main-wrapper').addClass('shrink');
+    $.ajax({
+        url: '//en.wikipedia.org/w/api.php',
+        data: { action: 'query', list: 'search', srsearch: $("input[name=wikipedia]").val(), format: 'json' },
+        dataType: 'jsonp',
+        success: function (x) {
+            console.log('title', x);
+              $('.articles-container').empty();
+            for (var i = 0; i < x.query.search.length; i++){
+              var contentHTML = '<a href=https://en.wikipedia.org/wiki/'+x.query.search[i].title+' target="_blank"><div class="articles-00 articles"><p class="title">'+x.query.search[i].title +'</p><p class="summary">'+x.query.search[i].snippet +'</p></div>'
+              $('.articles-container').append(contentHTML);
+              $('.articles-00').addClass('article');
+            }
+        }
+    });
+
   }
+
+
   $(this).addClass('search-focus');
   $(this).removeClass('search');
 
@@ -14,9 +31,12 @@ $('input').keyup(function() {
       $('.main-wrapper').removeClass('shrink');
       $('input').addClass('search');
       $('input').removeClass('search-focus');
+      $('.articles-container').empty();
+
 
     }
 });
+
 
 });
 
