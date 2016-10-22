d1c5820dd3d3dc27a5d55cc5a29d5c310fa52ee2
Karthik Meda <karthikreddymeda@gmail.com>
2016-10-23 02:24:58 +0530

    url title fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,7 +11,7 @@ $('input').keypress(function(ev){
             console.log('title', x);
               $('.articles-container').empty();
             for (var i = 0; i < x.query.search.length; i++){
-              var contentHTML = '<a href=https://en.wikipedia.org/wiki/'+x.query.search[i].title+' target="_blank"><div class="articles-00 articles"><p class="title">'+x.query.search[i].title +'</p><p class="summary">'+x.query.search[i].snippet +'</p></div>'
+              var contentHTML = '<a href=https://en.wikipedia.org/wiki/'+encodeURIComponent(x.query.search[i].title)+' target="_blank"><div class="articles-00 articles"><p class="title">'+x.query.search[i].title +'</p><p class="summary">'+x.query.search[i].snippet +'</p></div>'
               $('.articles-container').append(contentHTML);
               $('.articles-00').addClass('article');
             }
